324b00707dfc7fb86de0bd69fd4c8be4188ca571
LKing03x <Alexandre.venance03@gmail.com>
2023-06-19 15:28:40 +0000

    Add fix HotReload and HotRestart + Add support for multiple projects

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
+import os
+
 import sublime
 import sublime_plugin
 import subprocess
 
 
+def readFile(fn):
+    with open(fn, 'r') as fp:
+        return fp.read().strip()
+
+
+def getCurrentPID():
+    fn = sublime.active_window().active_view().file_name()
+    dir = os.path.dirname(fn)
+
+    for _ in range(12):
+        dirs = os.listdir(dir)
+        if 'flutter.pid' in dirs:
+            pid = readFile(os.path.join(dir, 'flutter.pid'))
+            return pid
+        else:
+            dir = os.path.dirname(dir)
+    return None
+
+
 class FlutterHotReloadCommand(sublime_plugin.WindowCommand):
 
     def run(self):
-        subprocess.call(["bash", "-c", ("kill -SIGUSR1 {0}").format(
-            int(subprocess.check_output(["cat", "./.flutter.pid"])))])
+        print(dir(self))
+        pid = getCurrentPID()
+        subprocess.call(["bash", "-c", "kill -SIGUSR1 {}".format(pid)])
 
 
 class FlutterHotRestartCommand(sublime_plugin.WindowCommand):
     def run(self):
-        subprocess.call(["bash", "-c", ("kill -SIGUSR2 {0}").format(
-            int(subprocess.check_output(["cat", "./.flutter.pid"])))])
+        pid = getCurrentPID()
+        subprocess.call(["bash", "-c", "kill -SIGUSR2 {}".format(pid)])
 
 
 class FlutterHotReloadWhenSaved(sublime_plugin.EventListener):
